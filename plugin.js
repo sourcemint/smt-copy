0e17c527752e1d2b7813ac037b7926c3e83d5d78
Christoph Dorn <christoph@christophdorn.com>
2015-05-01 19:08:08 -0700

    speedup

--- plugin.js
+++ plugin.js
@@ -5,186 +5,196 @@ exports.for = function (API) {
 
 	exports.turn = function (resolvedConfig) {
 
-		function copy (fromPath, toPath, callback) {
+		return API.ASYNC([
+			"GULP",
+			"GULP_DEBUG",
+			"GULP_PLUMBER",
+			"GULP_RENAME",
+			"GULP_REPLACE",
+			"GULP_EDIT"
+		], function (GULP, GULP_DEBUG, GULP_PLUMBER, GULP_RENAME, GULP_REPLACE, GULP_EDIT) {
 
-			API.console.debug("Copying and transforming fileset", fromPath, "to", toPath, "...");
+			function copy (fromPath, toPath, callback) {
 
-			var domain = require('domain').create();
-			domain.on('error', function(err) {
-				// The error won't crash the process, but what it does is worse!
-				// Though we've prevented abrupt process restarting, we are leaking
-				// resources like crazy if this ever happens.
-				// This is no better than process.on('uncaughtException')!
-				console.error("UNHANDLED DOMAIN ERROR:", err.stack, new Error().stack);
-				return callback(new Error("UNHANDLED DOMAIN ERROR"));
-			});
-			domain.run(function() {
+				API.console.debug("Copying and transforming fileset", fromPath, "to", toPath, "...");
 
-				try {
+				var domain = require('domain').create();
+				domain.on('error', function(err) {
+					// The error won't crash the process, but what it does is worse!
+					// Though we've prevented abrupt process restarting, we are leaking
+					// resources like crazy if this ever happens.
+					// This is no better than process.on('uncaughtException')!
+					console.error("UNHANDLED DOMAIN ERROR:", err.stack, new Error().stack);
+					return callback(new Error("UNHANDLED DOMAIN ERROR"));
+				});
+				domain.run(function() {
 
-					var destinationStream = null;
+					try {
 
-					destinationStream = API.GULP.dest(toPath);
+						var destinationStream = null;
 
-					destinationStream.once("error", function (err) {
-						return callback(err);
-					});
+						destinationStream = GULP.dest(toPath);
 
-					destinationStream.once("end", function () {
+						destinationStream.once("error", function (err) {
+							return callback(err);
+						});
 
-						API.console.debug("... done");
+						destinationStream.once("end", function () {
 
-						return callback();
-					});
+							API.console.debug("... done");
 
-					// TODO: Respect gitignore by making pinf walker into gulp plugin. Use pinf-package-insight to load ignore rules.
-					var stream = null;
-					stream = API.GULP.src([
-						"**",
-						".*"
-					], {
-						cwd: fromPath
-					});
+							return callback();
+						});
 
-					var lastPath = null;
-					stream
-						.pipe(API.GULP_PLUMBER())
+						// TODO: Respect gitignore by making pinf walker into gulp plugin. Use pinf-package-insight to load ignore rules.
+						var stream = null;
+						stream = GULP.src([
+							"**",
+							".*"
+						], {
+							cwd: fromPath
+						});
 
-					if (API.VERBOSE) {
+						var lastPath = null;
+						stream
+							.pipe(GULP_PLUMBER())
+
+						if (API.VERBOSE) {
+							stream = stream
+								.pipe(GULP_DEBUG({
+									title: '[smt-copy]',
+									minimal: true
+								}))
+						}
+
 						stream = stream
-							.pipe(API.GULP_DEBUG({
-								title: '[smt-copy]',
-								minimal: true
-							}))
-					}
-
-					stream = stream
-						.pipe(API.GULP_RENAME(function (path) {
-							var m = null;
-							if (
-								(m = path.basename.match(/__(.+?)__/)) &&
-								typeof resolvedConfig.variables[m[1]] !== "undefined"
-							) {
-								path.basename = path.basename.replace(new RegExp(m[0], "g"), resolvedConfig.variables[m[1]]);
-							} else
-							if (
-								(m = (path.basename + path.extname).match(/^__(.+?)__$/)) &&
-								typeof resolvedConfig.variables[m[1]] !== "undefined"
-							) {
-								path.basename = (path.basename + path.extname).replace(new RegExp(m[0], "g"), resolvedConfig.variables[m[1]]);
-								path.extname = "";
-							}
-							if (path.extname === ".tpl") {
-								var basename = path.basename.split(".");
-								path.extname = "." + basename.pop();
-								path.basename = basename.join(".");
-							}
-							lastPath = path.basename + path.extname;
-						}))
-						.pipe(API.GULP_REPLACE(/%%[^%]+%%/g, function (matched) {
-							var m = matched.match(/^%%([^%]+)%%$/);
-							if (
-								m &&
-								typeof resolvedConfig.variables[m[1]] !== "undefined"
-							) {
-								return resolvedConfig.variables[m[1]];
-							} else {
-								API.console.debug("Skip replacing '" + matched + "' in '" + lastPath + "' as variable not found in variables.");
-							}
-							return matched;
-						}))
-						.pipe(API.GULP_EDIT(function(newData, callback) {
-							var file = this;
-							return API.FS.exists(API.PATH.join(toPath, file.relative), function (exists) {
-								if (!exists) {
-									return callback(null, newData);
-								}
-								if (!resolvedConfig.onExists) {
-									return callback(null, newData);
-								}
-								var ext = file.relative.split(".").pop();
-								if (!resolvedConfig.onExists[ext]) {
-									return callback(null, newData);
-								}
-								if (/\.json$/.test(file.relative)) {
-									return API.FS.readFile(API.PATH.join(toPath, file.relative), "utf8", function (err, data) {
-										if (err) return callback(err);
-										if (resolvedConfig.onExists.json === "skip") {
-											return callback(null, data);
-										} else
-										if (resolvedConfig.onExists.json === "merge") {
-											try {
-												// What is already existing is more important than what is new.
-												newData = JSON.stringify(API.DEEPMERGE(JSON.parse(newData), JSON.parse(data)), null, 4);
-											} catch (err) {
-												err.message += " (while parsing JSON for '" + file.relative + "')";
-												err.stack += "\n(while parsing JSON for '" + file.relative + "')";
-												return callback(err);
-											}
-											return callback(null, newData);
-										} else {
-											throw new Error("'onExists.json' value of '" + resolvedConfig.onExists.json + "' not supported!");
-										}
-									});
-								} else
-								if (/\.md$/.test(file.relative)) {
-									return API.FS.readFile(API.PATH.join(toPath, file.relative), "utf8", function (err, data) {
-										if (err) return callback(err);
-										if (resolvedConfig.onExists.md === "skip") {
-											return callback(null, data);
-										} else {
-											throw new Error("'onExists.md' value of '" + resolvedConfig.onExists.md + "' not supported!");
-										}
-									});
-								} else
-								if (/\.js$/.test(file.relative)) {
-									return API.FS.readFile(API.PATH.join(toPath, file.relative), "utf8", function (err, data) {
-										if (err) return callback(err);
-										if (resolvedConfig.onExists.js === "skip") {
-											return callback(null, data);
-										} else {
-											throw new Error("'onExists.js' value of '" + resolvedConfig.onExists.js + "' not supported!");
-										}
-									});
+							.pipe(GULP_RENAME(function (path) {
+								var m = null;
+								if (
+									(m = path.basename.match(/__(.+?)__/)) &&
+									typeof resolvedConfig.variables[m[1]] !== "undefined"
+								) {
+									path.basename = path.basename.replace(new RegExp(m[0], "g"), resolvedConfig.variables[m[1]]);
 								} else
 								if (
-									typeof resolvedConfig.onExists[ext] === "object",
-									resolvedConfig.onExists[ext].action === "wrap"
+									(m = (path.basename + path.extname).match(/^__(.+?)__$/)) &&
+									typeof resolvedConfig.variables[m[1]] !== "undefined"
 								) {
-									return API.FS.readFile(API.PATH.join(toPath, file.relative), "utf8", function (err, data) {
-										if (err) return callback(err);
-										// Check if we have already wrapped it by comparing the first 3 lines of the file.
-										if (
-											data.split("\n").slice(0, 3).join("\n") === newData.split("\n").slice(0, 3).join("\n")
-										) {
-											// Already wrapped.
-											newData = data;
-										} else {
-											newData = newData.replace(new RegExp(API.ESCAPE_REGEXP_COMPONENT(resolvedConfig.onExists[ext].anchor), "g"), data);
-										}
-										return callback(null, newData);
-									});
+									path.basename = (path.basename + path.extname).replace(new RegExp(m[0], "g"), resolvedConfig.variables[m[1]]);
+									path.extname = "";
 								}
-								var err = new Error("Implement instruction parsing for file: " + file.relative);
-								console.error(err.stack);
-								return callback(err);
-							});
-					    }))
-//						.pipe(filter.restore())									
-						.pipe(destinationStream);
+								if (path.extname === ".tpl") {
+									var basename = path.basename.split(".");
+									path.extname = "." + basename.pop();
+									path.basename = basename.join(".");
+								}
+								lastPath = path.basename + path.extname;
+							}))
+							.pipe(GULP_REPLACE(/%%[^%]+%%/g, function (matched) {
+								var m = matched.match(/^%%([^%]+)%%$/);
+								if (
+									m &&
+									typeof resolvedConfig.variables[m[1]] !== "undefined"
+								) {
+									return resolvedConfig.variables[m[1]];
+								} else {
+									API.console.debug("Skip replacing '" + matched + "' in '" + lastPath + "' as variable not found in variables.");
+								}
+								return matched;
+							}))
+							.pipe(GULP_EDIT(function(newData, callback) {
+								var file = this;
+								return API.FS.exists(API.PATH.join(toPath, file.relative), function (exists) {
+									if (!exists) {
+										return callback(null, newData);
+									}
+									if (!resolvedConfig.onExists) {
+										return callback(null, newData);
+									}
+									var ext = file.relative.split(".").pop();
+									if (!resolvedConfig.onExists[ext]) {
+										return callback(null, newData);
+									}
+									if (/\.json$/.test(file.relative)) {
+										return API.FS.readFile(API.PATH.join(toPath, file.relative), "utf8", function (err, data) {
+											if (err) return callback(err);
+											if (resolvedConfig.onExists.json === "skip") {
+												return callback(null, data);
+											} else
+											if (resolvedConfig.onExists.json === "merge") {
+												try {
+													// What is already existing is more important than what is new.
+													newData = JSON.stringify(API.DEEPMERGE(JSON.parse(newData), JSON.parse(data)), null, 4);
+												} catch (err) {
+													err.message += " (while parsing JSON for '" + file.relative + "')";
+													err.stack += "\n(while parsing JSON for '" + file.relative + "')";
+													return callback(err);
+												}
+												return callback(null, newData);
+											} else {
+												throw new Error("'onExists.json' value of '" + resolvedConfig.onExists.json + "' not supported!");
+											}
+										});
+									} else
+									if (/\.md$/.test(file.relative)) {
+										return API.FS.readFile(API.PATH.join(toPath, file.relative), "utf8", function (err, data) {
+											if (err) return callback(err);
+											if (resolvedConfig.onExists.md === "skip") {
+												return callback(null, data);
+											} else {
+												throw new Error("'onExists.md' value of '" + resolvedConfig.onExists.md + "' not supported!");
+											}
+										});
+									} else
+									if (/\.js$/.test(file.relative)) {
+										return API.FS.readFile(API.PATH.join(toPath, file.relative), "utf8", function (err, data) {
+											if (err) return callback(err);
+											if (resolvedConfig.onExists.js === "skip") {
+												return callback(null, data);
+											} else {
+												throw new Error("'onExists.js' value of '" + resolvedConfig.onExists.js + "' not supported!");
+											}
+										});
+									} else
+									if (
+										typeof resolvedConfig.onExists[ext] === "object",
+										resolvedConfig.onExists[ext].action === "wrap"
+									) {
+										return API.FS.readFile(API.PATH.join(toPath, file.relative), "utf8", function (err, data) {
+											if (err) return callback(err);
+											// Check if we have already wrapped it by comparing the first 3 lines of the file.
+											if (
+												data.split("\n").slice(0, 3).join("\n") === newData.split("\n").slice(0, 3).join("\n")
+											) {
+												// Already wrapped.
+												newData = data;
+											} else {
+												newData = newData.replace(new RegExp(API.ESCAPE_REGEXP_COMPONENT(resolvedConfig.onExists[ext].anchor), "g"), data);
+											}
+											return callback(null, newData);
+										});
+									}
+									var err = new Error("Implement instruction parsing for file: " + file.relative);
+									console.error(err.stack);
+									return callback(err);
+								});
+						    }))
+	//						.pipe(filter.restore())									
+							.pipe(destinationStream);
 
-					return stream.once("error", function (err) {
-						err.message += " (while running gulp)";
-						err.stack += "\n(while running gulp)";
+						return stream.once("error", function (err) {
+							err.message += " (while running gulp)";
+							err.stack += "\n(while running gulp)";
+							return callback(err);
+						});
+					} catch (err) {
 						return callback(err);
-					});
-				} catch (err) {
-					return callback(err);
-				}
-			});
-		}
+					}
+				});
+			}
 
-		return API.Q.denodeify(copy)(resolvedConfig.fromPath, resolvedConfig.toPath);
+			return API.Q.denodeify(copy)(resolvedConfig.fromPath, resolvedConfig.toPath);
+		});
 	}
 
 	return exports;
